d0a2d053feb8b45a6d16cb42ba9ae111aba681b4
KATHIRAVAN PERIYATHAMBI <jothikathiravan2@gmail.com>
2023-08-29 12:58:08 +0530

    first

--- src/main/java/com/hello/hello/Myconfig.java
+++ src/main/java/com/hello/hello/Myconfig.java
@@ -1,6 +1,11 @@
 package com.hello.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Myconfig {
+	@GetMapping
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	}
