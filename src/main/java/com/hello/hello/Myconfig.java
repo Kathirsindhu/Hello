124ef765fe54507a47e359d78f88dc5149febe3f
Kathiravan <117353870+Kathirsindhu@users.noreply.github.com>
2023-08-29 13:06:41 +0530

    Update Myconfig.java

--- src/main/java/com/hello/hello/Myconfig.java
+++ src/main/java/com/hello/hello/Myconfig.java
@@ -1,17 +1,10 @@
 package com.hello.hello;
-<<<<<<< HEAD
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Myconfig {
-	@GetMapping
-=======
 @RestController
 public class Myconfig {
 	@GetMapping("/hello")
->>>>>>> b7d2ae4730e249ef1cd49b95df2d7763ff32b665
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	}
